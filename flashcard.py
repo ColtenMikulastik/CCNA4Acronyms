2e7ead83194d8fb74881992a4ab5021a0ddd89fa
Colten <mikulastikc@gmail.com>
2022-04-23 15:18:57 -0500

    added flashcard functionality, and removed name from top for now

--- flashcard.py
+++ flashcard.py
@@ -1,6 +1,16 @@
 # the idea is that you should be able to squentially see card and then after input show it's definition
 
-
-print("Hello")
+# I need to read the information from  the file so... yeah
+with open("Acronyms.txt", 'r') as file:
+    # now I have to go line by line reading eveything in file
+    for line in file.readlines():
+        #split the word and the definition based on a colon, and then set their index's to variables
+        line = line.split(':')
+        Acro = line[0]
+        Def = line[1]
+        print(Acro)
+        input("press space when ready to see definition")
+        print(Def)
+        
 
 
